c069c7bb38b41ea385a79a731930f63d886d3a63
prashant-raghu <prashantraghu999@gmail.com>
2020-07-27 21:53:53 +0530

    tracingService

---
+++ services/tracing.service.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"context"
+	"net/http"
+)
+
+func Tracing(nextRequestID func() string, requestIDKey int) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			requestID := r.Header.Get("X-Request-Id")
+			if requestID == "" {
+				requestID = nextRequestID()
+			}
+			ctx := context.WithValue(r.Context(), requestIDKey, requestID)
+			w.Header().Set("X-Request-Id", requestID)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
